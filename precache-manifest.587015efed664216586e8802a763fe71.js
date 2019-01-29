dd0ecc51d25c048c261c2d459a49b8daa7ae54a2
Keo <kthorng@gmail.com>
2019-01-29 14:51:12 -0800

    Updates

--- precache-manifest.587015efed664216586e8802a763fe71.js
+++ precache-manifest.587015efed664216586e8802a763fe71.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
   },
   {
-    "revision": "8affabc8052b34164141",
-    "url": "/react-portfolio/static/js/main.8affabc8.chunk.js"
+    "revision": "848e7545181a81240bb2",
+    "url": "/react-portfolio/static/js/main.848e7545.chunk.js"
   },
   {
     "revision": "8c5cdce93909f918cb62",
     "url": "/react-portfolio/static/js/1.8c5cdce9.chunk.js"
   },
   {
-    "revision": "99da73ad6357c4ecf23b55a145f74192",
+    "revision": "b3cc502e17c51a8f1f602d3e7289da54",
     "url": "/react-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.b93225178dc14ca448cad8b17681c7da.js"
+  "/react-portfolio/precache-manifest.587015efed664216586e8802a763fe71.js"
 );
 
 workbox.clientsClaim();
